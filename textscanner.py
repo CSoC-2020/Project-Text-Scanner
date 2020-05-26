a8670c4f17f297201a1263bace269e656aa83bdf
Ajay Singh Bhandari <64245706+Ajey0077@users.noreply.github.com>
2020-05-26 16:29:12 +0530

    Update textscanner.py

--- textscanner.py
+++ textscanner.py
@@ -9,53 +9,116 @@ from tkinter import simpledialog
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 
-def main_scan():
+def Import():
     a = askopenfilename(title="import")
-    ext=os.path.splitext(a)[1]     # it split extension from file location
-    #print(" hello ",ext)
+    ext = os.path.splitext(a)[1]  # it split extension from file location
+    # print(" hello ",ext)
     a_name = a.split("/")[::-1]
     a_name = a_name[0].split(".")
 
-    #--- extracting from image---
+    # --- extracting from image---
 
-    if ext==".png" or ext==".jpg":
+    if ext == ".png" or ext == ".jpg":
         img = Image.open(a)
         txt = pytesseract.image_to_string(img)
         print(txt)
-        b = f" {a_name[0]}.txt"
+        b = f"extracted_txt\{a_name[0]}.txt"
         with open(b, "w") as f:
             f.write(txt)
 
-    #---extracting from PDF file
+    # ---extracting from video file
 
-    elif ext==".pdf":
-        pdf=PdfFileReader(a)
+    elif ext == ".mp4" or ext == ".avi" or ext == ".mkv":
+        cam = cv2.VideoCapture(a)
+
+        try:
+
+            # creating a folder named vid-frames
+            if not os.path.exists('vid_frames'):
+                os.makedirs('vid_frames')
+
+            # if not created then raise error
+        except OSError:
+            print('Error: Creating directory of data')
+        i = 0
+
+        # counter for reducing no. of frames
+        counter = 0
+        frame_skip = 10
+        txt = ''
+        txt1 =''
+
+        # frame
+        while cam.isOpened():
+
+            # reading from frame
+            ret, frame = cam.read()
+
+            if not ret:
+                break
+            if counter > frame_skip - 1:
+                # if video is still left continue creating images
+                name = './vid_frames/frame' + str(i) + '.jpg'
+                print('Creating...' + name)
+                cv2.imwrite(name, frame)
+                img = Image.open(name)
+
+                txt1 = pytesseract.image_to_string(img)
+                if txt1 not in txt:
+                    txt = txt + " \n " + txt1
+                else:
+                    pass
+                # this counter will moniter no. of frames created
+                i += 1
+
+                counter = 0
+                continue
+            counter += 1
+        print("text : ", txt)
+
+        # Release all space and windows once done
+        cam.release()
+        cv2.destroyAllWindows()
+
+        b = f"extracted_txt\{a_name[0]}.txt"
+        with open(b, "w") as f:
+            f.write(txt)
+
+
+
+    # ----- extraction from pdf file
+
+    elif ext == ".pdf":
+        pdf = PdfFileReader(a)
         with open(f"{a_name[0]}.txt", "w") as f:
             for page_num in range(pdf.numPages):
                 print(f"page : {page_num}")
                 pageobj = pdf.getPage(page_num)
 
                 try:
+                    # extracting text from single page
                     txt = pageobj.extractText()
                     print(''.center(100, "-"))
                 except:
                     pass
                 else:
+                    # page indexing
                     f.write(f"page {page_num + 1}\n")
                     f.write(" ".center(100, "-"))
                     f.write(txt)
             f.close()
 
+    else:
+        print("extraction of text cannot be performed of this extension")
 
 
-
-def Take():
-    txt=""
+def Picture():
+    txt = ""
     cam = cv2.VideoCapture(0)
 
     cv2.namedWindow("Camera")
 
-    counter= 0
+    counter = 0
 
     while True:
         ret, frame = cam.read()
@@ -72,34 +135,148 @@ def Take():
             # press SPACE to save
             img_name = f"picture{counter}.png"
             cv2.imwrite(img_name, frame)
-            new_name=img_name.split(".")
+            new_name = img_name.split(".")
             print(f"{new_name[0]} taken!")
             img = Image.open(img_name)
-            txt = txt+pytesseract.image_to_string(img)
+            txt = txt + pytesseract.image_to_string(img)
             counter += 1
     cam.release()
     cv2.destroyAllWindows()
 
     print(txt)
-    b = f"{new_name[0]}.txt"
+    b = f"extracted_txt\{new_name[0]}.txt"
+    with open(b, "w") as f:
+        f.write(txt)
+
+
+
+def Video():
+    # making directory for output video
+    try:
+
+        # creating a folder named vid_frames
+        if not os.path.exists('captured_video'):
+            os.makedirs('captured_video')
+
+        # if not created then raise error
+    except OSError:
+        print('first create captured_video folder manullay')
+
+   #recording video
+    cam = cv2.VideoCapture(0)
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('captured_video\output.avi',fourcc, 20.0, (640,480))
+
+    while(True):
+        ret, frame = cam.read()
+        out.write(frame)
+        cv2.imshow('capture_video',frame)
+        if cv2.waitKey(1) == 27:
+            print("camera is closed")
+            break
+# Release everything if job is finished
+
+    cam.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+# extracting text from video
+
+    cam1 = cv2.VideoCapture("captured_video\output.avi")
+
+    try:
+
+        # creating a folder named vid_frames
+        if not os.path.exists('vid_frames'):
+            os.makedirs('vid_frames')
+
+        # if not created then raise error
+    except OSError:
+        print('Error: Creating directory of data')
+    i=0
+    # frame
+    counter = 0
+    frame_skip=10
+    txt=''
+    txt1=''
+    while cam1.isOpened():
+
+        # reading from frame
+        ret, frame = cam1.read()
+
+        if not ret:
+            break
+        if counter > frame_skip - 1:
+            # if video is still left continue creating images
+            name = './vid_frames/frame' + str(i) + '.jpg'
+            print('Creating...' + name)
+            cv2.imwrite(name, frame)
+            img = Image.open(name)
+
+            txt1=pytesseract.image_to_string(img)
+            if txt1 not in txt:
+                txt=txt+" \n "+txt1
+            else:
+                pass
+            # writing the extracted images
+            i+=1
+            counter=0
+            continue
+            # increasing counter so that it will
+            # show how many frames are created
+        counter += 1
+    print("text : ",txt)
+
+    # Release all space and windows once done
+    cam1.release()
+    cv2.destroyAllWindows()
+
+    b = "extracted_txt\output.txt"
     with open(b, "w") as f:
         f.write(txt)
 
 
 
 
+def Take():
+    screen1 = Toplevel(screen)
+    screen1.geometry("800x800")
+    screen1.title("Take")
+    Label(screen1,text="choose one ").pack()
+    Label(screen1,text=10*"\n").pack()
+    Button(screen1,text="Picture",bg="grey",height="4",width="45",command=Picture).pack()
+    Label(screen1,text=" Press space bar to take pictures and ESC to exit  ").pack()
+    Label(screen1,text=5*"\n").pack()
+    Button(screen1,text="Video",bg="grey",height="4",width="45",command=Video).pack()
+    Label(screen1,text="Press ESC button to exit from video").pack()
+
+
+
 def main_screen():
+    # creating directory for text file
+    try:
+
+        # creating a folder named vid_frames
+        if not os.path.exists('extracted_txt'):
+            os.makedirs('extracted_txt')
+
+        # if not created then raise error
+    except OSError:
+        print('Error: first create dir for text file extracted_txt manually')
+
+    # making GUI
     global screen
     screen = Tk()
-    screen.geometry("600x600")
+    screen.geometry("800x800")
     screen.title("TEXT SCANNER")
     Label(text="Pick your option").pack()
-    Label(text=8 * "\n ").pack()
-    Button(text="Import",bg="grey", height="2", width="30", command=main_scan).pack()
-    Label(text=" ").pack()
-    Button(text="Take",bg="grey", height="2", width="30", command=Take).pack()
-    Label(text=5* "\n").pack()
-    Label(text="Tip : use SPACE BAR to take picture and ESC to close camera.").pack()
+    Label(text=10* "\n ").pack()
+    Button(text="Import", bg="grey", height="4", width="45", command=Import).pack()
+    Label(text=5*"\n ").pack()
+    Button(text="Take", bg="grey", height="4", width="45", command=Take).pack()
     screen.mainloop()
 
 main_screen()
+
